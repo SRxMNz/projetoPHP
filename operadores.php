791fc050f1e532c477ff43fb5b8785f8466b1fd3
SRxMNz <edna.32limalima@gmail.com>
2025-08-19 19:46:41 -0300

    trabalhando com operadores em php

---
+++ operadores.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo("Trabalhando com operadores em PHP");
+echo("operadores de soma <br/> ");
+echo("operadores de subtração <br/> ");
+echo("operadores de divisão <br/> ");
+echo("operadores de multiplicação <br/> ");
+
+
+ ?>
