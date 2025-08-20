07633ca63f181a452076e3db48e27b8d73c0bc1d
SRxMNz <edna.32limalima@gmail.com>
2025-08-19 22:16:24 -0300

    sla kkk

--- operadores.php
+++ operadores.php
@@ -1,13 +1,31 @@
 <?php 
 
-echo("Trabalhando com operadores em PHP");
+echo("Trabalhando com operadores em PHP <br/>");
 echo("operadores de soma <br/> ");
 echo("operadores de subtração <br/> ");
 echo("operadores de divisão <br/> ");
 echo("operadores de multiplicação <br/> ");
 
 
-echo "insira a operação desejada <br/>";
+echo "insira a operação desejada <br/> <br/>";
 
+$num1=10;
+$num2=2;
+
+// soma
+$resp = $num1 + $num2;
+echo("A soma de $num1 e $num2 é: $resp  <br/>");
+
+// subtração
+$resp = $num1 - $num2;
+echo("A soma de $num1 e $num2 é: $resp  <br/>");
+
+// multiplicação
+$resp = $num1 * $num2;
+echo("A soma de $num1 e $num2 é: $resp  <br/>");
+
+// divisão
+$resp = $num1 / $num2;
+echo("A soma de $num1 e $num2 é: $resp  <br/>");
 
  ?>
